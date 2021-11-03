5a31aea32f9cb566a1b038d7f3f81d8d561020f8
Dragos Campean <dragos@sectorlabs.ro>
2021-11-03 10:52:38 +0200

    Update deps

--- someapp/e2e/test/dummy.desktop.test.js
+++ someapp/e2e/test/dummy.desktop.test.js
@@ -1,6 +1,6 @@
 describe('Some dummy test', () => {
     step('should open page', () => {
         browser.url("https://zameen.com");
-        // browser.cdp('Profiler', 'enable');
+        browser.cdp('Profiler', 'enable');
     });
 });
